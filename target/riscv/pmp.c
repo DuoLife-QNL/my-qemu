0bcdaca8696f583b03592c24fe9f01494cd02721
Myriad-Dreamin <camiyoru@gmail.com>
2020-05-15 10:01:36 +0800

    fix update mseccfg.RLB: wrong iter range

--- target/riscv/pmp.c
+++ target/riscv/pmp.c
@@ -420,7 +420,7 @@ void mseccfg_csr_write(CPURISCVState *env, target_ulong val)
     int i;
 
     if (!(env->mseccfg & PMP_MSECCFG_RLB)) {
-        for (i = 0; i < env->pmp_state.num_rules; i++) {
+        for (i = 0; i < MAX_RISCV_PMPS; i++) {
             if (pmp_is_locked(env, i)) {
                 val &= ~PMP_MSECCFG_RLB;
                 val |= (env->mseccfg & PMP_MSECCFG_RLB);
